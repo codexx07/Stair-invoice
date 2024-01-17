cc836691c28900e0ec70b18182b9a8345b439a7b
codexx07 <sparsh.riju@gmail.com>
2024-01-17 21:39:56 +0530

    Idfy working but showing in progress

--- src/server/app.py
+++ src/server/app.py
@@ -30,16 +30,43 @@
 # print(response.text)
 
 import requests
+import uuid
+import json
 
+# POST request
 url = "https://eve.idfy.com/v3/tasks/async/verify_with_source/udyam_aadhaar"
 
-payload = "{\n  \"task_id\": \"74f4c926-250c-43ca-9c53-453e87ceacd1\",\n  \"group_id\": \"8e16424a-58fc-4ba4-ab20-5bc8e7c3c41e\",\n  \"data\": {\n    \"uam_number\": \"UDYAM-MH-33-0006866\"\n   }\n}"
+task_id = str(uuid.uuid4())
+group_id = str(uuid.uuid4())
+
+payload = {
+    "task_id": task_id,
+    "group_id": group_id,
+    "data": {
+        "uam_number": "UDYAM-MH-33-0006866"
+    }
+}
+
 headers = {
     'api-key': '4c71388e-79d5-4c84-a111-a8f35a93605e',
     'account-id': '6d014c04f254/b95e280a-0c6c-47ac-b518-70b89cd82f88',
-    'User-Agent': 'PostmanRuntime/7.28.4'
 }
 
-response = requests.request("POST", url, headers=headers, data=payload)
+headers2 ={
+    'api-key': '4c71388e-79d5-4c84-a111-a8f35a93605e',
+    'account-id': '6d014c04f254/b95e280a-0c6c-47ac-b518-70b89cd82f88',
+    'Content-Type': 'application/json'
+}
 
-print(response.text)
+response = requests.request("POST", url, headers=headers, json=payload)
+
+# Extract request_id from the response
+response_data = json.loads(response.text)
+request_id = response_data.get('request_id')
+
+# GET request
+get_url = f"https://eve.idfy.com/v3/tasks?request_id={request_id}"
+
+get_response = requests.request("GET", get_url, headers=headers2)
+
+print(get_response.text)
